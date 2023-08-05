334d4416989e8561fa294e0b0a47ed2d211cb8b2
dhela19 <ubongabasijerome8@gmail.com>
2023-08-05 23:59:17 +0100

    day 1

---
+++ casting.py
@@ -0,0 +1,43 @@
+"""
+The common data types are:
+
+Integers,
+Floats,
+Strings,
+Lists,
+Tuples,
+Dictionaries,
+Sets,
+Booleans
+"""
+
+#Casting
+
+x = str(3) # x = '3'
+y = int(5) # y = 5
+z = float(3) # z = 3.0
+a = 3.178
+
+#print(z)
+#print(x + y)
+
+
+
+#Get the data type
+"""
+You can get the data type of a variable 
+using the type() function
+"""
+print(type(a))
+
+
+#Case Sensitive
+A = 4.267
+
+
+#Strings are enclosed in single or double quotes
+b = 'the tutorial will close in'
+c = 'minutes'
+
+v = str(b) + float(A) + str(c)
+print(v)
---
+++ comments.py
@@ -0,0 +1,8 @@
+print("Hello World") #this line will print hello world
+"""
+Line two and three
+assigns values to a variable
+"""
+x = 3 
+y = 5 
+print(x+y) #this line prints the sum of x and y 
