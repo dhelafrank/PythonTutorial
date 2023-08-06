e43322f6d957cd2da4bcfe10acbcb8f7d98c1df5
dhela19 <ubongabasijerome8@gmail.com>
2023-08-06 21:53:17 +0100

    Day 2

---
+++ DataType(strings).py
@@ -0,0 +1,8 @@
+'''
+strings are alphanumeric chracters that are surrounded by 
+either single or double quotes
+'''
+
+myString = "Tutorial"
+print(myString)
+
--- variables.py
+++ variables.py
@@ -1,3 +1,63 @@
 x = 5
 y = "Hello World"
 print(y)
+
+# print = "Hello World"
+
+# Camelcase
+# myVariableName = "Hello World 2"
+# print(myVariableName)
+
+# snakecase
+my_variable_name = "Hello World 3"
+print(my_variable_name)
+
+# no reserved keywords for variable declaration
+
+age, height, length = 18, 7.1, 6
+
+print(age)
+print(height)
+
+# unpacking
+fruits = ["apple", 'banana', "cherry"]
+print(fruits[1])
+# firstF, secondF, thirdF = fruits
+# print(firstF)
+# print(secondF)
+
+
+# assigning a single value to multiple variables
+green = big = Fruits = "mango"
+print(green)
+print(big)
+
+
+# output variables
+text = "Python is awesome"
+secondText = "you should learn it"
+print(text, secondText)
+
+
+value1 = str(1) #casting
+value2 = "string"
+print(value1+value2)
+
+
+#variable scope
+
+'''
+functions are basically a block of code that can be used
+to excute or perform tasks
+'''
+
+
+#make a scoped variable global with the global [vairbale name]
+#keyword
+def myFunction():
+    global functVar1
+    functVar1 = "First function variable"
+    print(functVar1)
+
+myFunction()
+print(functVar1)
